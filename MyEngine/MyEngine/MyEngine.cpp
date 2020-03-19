d0d7640fb231dae00ec67bc967855482a798a15b
Ella Zhang <ellazhang37@gmail.com>
2020-03-18 21:19:30 -0400

    Hide the face that we cannot see

--- MyEngine/MyEngine/MyEngine.cpp
+++ MyEngine/MyEngine/MyEngine.cpp
@@ -133,15 +133,38 @@ public:
 			MultiplyMatrixVector(triRotatedZ.p[1], triRotatedZX.p[1], matRotX);
 			MultiplyMatrixVector(triRotatedZ.p[2], triRotatedZX.p[2], matRotX);
 
-
+			// Offset into the screen
 			triTranslated = triRotatedZX;
 			triTranslated.p[0].z = triRotatedZX.p[0].z + 3.0f;
 			triTranslated.p[1].z = triRotatedZX.p[1].z + 3.0f;
 			triTranslated.p[2].z = triRotatedZX.p[2].z + 3.0f;
 
+			vec3d normal, line1, line2;
+			line1.x = triTranslated.p[1].x - triTranslated.p[0].x;
+			line1.y = triTranslated.p[1].y - triTranslated.p[0].y;
+			line1.z = triTranslated.p[1].z - triTranslated.p[0].z;
+
+			line2.x = triTranslated.p[2].x - triTranslated.p[0].x;
+			line2.y = triTranslated.p[2].y - triTranslated.p[0].y;
+			line2.z = triTranslated.p[2].z - triTranslated.p[0].z;
+
+			normal.x = line1.y * line2.z - line1.z * line2.y;
+			normal.y = line1.z * line2.x - line1.x * line2.z;
+			normal.z = line1.x * line2.y - line1.y * line2.x;
+
+			float l = sqrtf(normal.x * normal.x + normal.y * normal.y + normal.z * normal.z);
+			normal.x / l;
+			normal.y / l;
+			normal.z / l;
+
+
+			if (normal.z < 0)
+			{
+			// Project triangles from 3D to 2D ( from the world space to the screen space)
 			MultiplyMatrixVector(triTranslated.p[0], triProjected.p[0], matProj);
 			MultiplyMatrixVector(triTranslated.p[1], triProjected.p[1], matProj);
 			MultiplyMatrixVector(triTranslated.p[2], triProjected.p[2], matProj);
+			}
 
 			// Scale into view
 			triProjected.p[0].x += 1.0f;
